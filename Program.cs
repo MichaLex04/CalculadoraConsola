8ffeae478ce51949734844a950281a9f6a827920
MichaLex04 <Sen4lex@gmail.com>
2025-05-07 09:10:40 -0500

    a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Calculadora calc = new();
+Console.WriteLine("Ingrese N1");
+int n1=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese N2");
+int n2=int.Parse(Console.ReadLine());
+Console.WriteLine("Seleccione: SUMA S/nRESTA R/nMULTIPLICACION M/nDIVISION D");
+string opcion=Console.ReadLine();
+int res = opcion.equals("S") ? calc.Sumar(n1, n2) : 
+          opcion.equals("R") ? calc.Restar(n1, n2) :
+          opcion.equals("M") ? calc.Multiplicar(n1, n2) :
+          opcion.equals("D") ? calc.Dividir(n1, n2) : 0;
+Console.WriteLine("RESPUESTA "+res);
