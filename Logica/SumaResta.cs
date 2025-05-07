851101c44041932a8c0334ac09230347bee11468
fabian <fabianvaleriano53@gmail.com>
2025-05-07 09:06:16 -0500

    votame tu gaaaaa

---
+++ Logica/SumaResta.cs
@@ -0,0 +1,9 @@
+namespace CalculadoraConsola.Logica
+{
+    public class SumaResta
+    {
+        public int Suma(int n1 , int n2)=> n1+n2;
+
+        public int Resta (int n2 , int n2)=> n1-n2;
+    }
+}
