e836a37a20ea54e1f5cba652e74ba4a06e5a930c
jhoel <juniorgarciapro2020@gamil.com>
2025-05-07 09:06:28 -0500

    datos guardados

---
+++ Logica/DiviMulti.cs
@@ -0,0 +1,10 @@
+namespace CalculadoraConsola.Logica
+{
+    public class DiviMulti
+    {
+        public int MULTIPLICACION (int n1 , int n2) => n1 * n2;
+
+        public int DIVICION (int n1 , int n2) => n1 / n2;
+
+    }
+}
